95041451761d165ad15ff80649bf8bb74625b0cb
AnisaAisha <35626637+AnisaAisha@users.noreply.github.com>
2020-01-07 02:41:44 +0500

    Implemented asynchronous frame buffer

--- Framebuffer/grid.py
+++ Framebuffer/grid.py
@@ -21,42 +21,45 @@ red = (255, 0, 0)
 running = True
 refresh = False
 
+#update after 60 second interval
+DISPLAY = pygame.USEREVENT + 1
+pygame.time.set_timer(DISPLAY, 60000)
 
 while running:
-    
-                
+      
     # process inputs(events)
     for event in pygame.event.get():
         # check for closing window
         if event.type == pygame.QUIT:
             running = False
-        elif event.type == pygame.KEYDOWN:
-            #pressing R key refreshes screen
-            if event.key == pygame.K_r:
-                print('Refresh screen')
-                refresh = True
+        elif event.type == DISPLAY and not refresh:
+            print('Refresh screen')
+            refresh = True
                 
-                #get buffer info
-                s = pygame.surfarray.pixels3d(screen)
+            #get buffer info
+            s = pygame.surfarray.pixels3d(screen)
                 
-                #update buffer info
-                for i in range(len(s)):
-                    for j in range(len(s[i])):
-                        if np.all(s[i][j]):
-                            s[i][j] = [255,0,0]
-                out = pygame.surfarray.make_surface(s)
-                del s
-                screen.blit(out, (0,0))
-                print('done')
-                
+            #update buffer info
+            for i in range(len(s)):
+                for j in range(len(s[i])):
+                    if np.all(s[i][j]):
+                        s[i][j] = [255,0,0]
+            out = pygame.surfarray.make_surface(s)
+            del s
+            screen.blit(out, (0,0))
+            print('done')
+            
+        elif event.type == DISPLAY and refresh:
+            refresh = False
+            
     #draw grid
-    if refresh == False:
+    if not refresh:
         screen.fill(black)
         for y in range(margin, HEIGHT, width+margin):
             for x in range(margin, HEIGHT, width+margin):
                 rect = pygame.Rect(y, x, block_size, block_size)
                 pygame.draw.rect(screen, white, rect)
-    
+
     pygame.display.update()
     
 pygame.quit()
