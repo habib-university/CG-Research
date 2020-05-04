1abcb7e73db4101e40452105537200ab7b0edb92
kr03917 <31242820+kr03917@users.noreply.github.com>
2020-05-05 02:32:06 +0500

    Completed primitive class

--- Pipeline/primitiveClass.py
+++ Pipeline/primitiveClass.py
@@ -1,8 +1,10 @@
 class Point:
-    def __init__(self):
+    def __init__(self,xVal,yVal):
+        self.x = xVal
+        self.y = yVal
         #By default it is 1, and needs to be enabled
         self.pointSize= 1
-        
+    
 class Line:
     def __init__(self):
 ##        self.x0
@@ -32,4 +34,48 @@ class Line:
         print(self.lineArray)
 
 
-
+class Triangle:
+    def __init__(self):
+        self.triangleArray = []
+    def GL_TRIANGLES(self,vertices):
+        if (len(vertices)>2):
+            if (len(vertices)%3!=0):
+                print("Not enough values, last",len(vertices)%3,"value(s) popped")
+                for i in range(0,len(vertices)%3):
+                    vertices.pop(-1)
+            for i in range(0,len(vertices),3):
+                self.triangleArray.append([[vertices[i]],[vertices[i+1]],[vertices[i+2]]])
+        else:
+            print("Not enough values")
+        
+    def GL_TRIANGLE_STRIP(self, vertices):        
+        #Checking if the first face is clockwise or anticlockwise
+        if (len(vertices)>2):
+            faceDirection = self.clockwiseFace(vertices[0],vertices[1],vertices[2])
+            for i in range(0,len(vertices)-2):
+                if (self.clockwiseFace(vertices[i],vertices[i+1],vertices[i+2]) == faceDirection):
+                    self.triangleArray.append([[vertices[i]],[vertices[i+1]],[vertices[i+2]]])
+                else:
+                    self.triangleArray.append([[vertices[i+1]],[vertices[i]],[vertices[i+2]]])
+        else:
+            print("Not enough values")
+    def GL_TRIANGLE_FAN(self,vertices):
+        if (len(vertices)>2):
+            for i in range(1,len(vertices)-1,1):
+                self.triangleArray.append([vertices[0],vertices[i],vertices[i+1]])
+        else:
+            print("Not enough values")
+    def printTriArray(self):
+        for i in range(0,len(self.triangleArray)):
+            print(self.triangleArray[i])
+            
+    def clockwiseFace(self,a,b,c):
+        #AntiClockWise
+        if (b[0] - a[0])*(c[1]-a[1])-(c[0]-a[0])*(b[1]-a[1])> 0:
+            return "Counterclockwise"
+        #Clockwise
+        elif (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1])<0:
+            return "Clockwise"
+        else:
+            return "Linear"
+        
--- Pipeline/unitTesting.py
+++ Pipeline/unitTesting.py
@@ -2,7 +2,7 @@ from bressenham import *
 from primitiveClass import *
 def checkBresenham():
     print(BresLineAlgorithm(20,10,30,18))
-checkBresenham()
+#checkBresenham()
 
 def checkLineFunctions():
     ##line=Line()
@@ -17,3 +17,15 @@ def checkLineFunctions():
     lineloop=Line()
     lineloop.GL_LINE_LOOP([[1,1],[2,2],[3,3],[4,4],[5,5],[6,6]])
     lineloop.printLines()
+
+def checkClockwise():
+    tri = Triangle()
+    #print(tri.clockwiseFace([5,5],[0,0],[5,0]))
+    vertices = [[1,10],[0,8],[3,10],[2,7],[5,8],[3,5],[6,6],[5,3],[7,4]]
+    #tri.GL_TRIANGLES(vertices)
+    #tri.printTriArray()
+    tri.GL_TRIANGLE_STRIP(vertices)
+    tri.printTriArray()
+    #tri.GL_TRIANGLE_FAN(vertices)
+    #tri.printTriArray()
+checkClockwise()
